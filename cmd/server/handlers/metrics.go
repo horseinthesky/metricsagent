410ca1bbfb2e0c6eab7322a5ddc6cca93366b652
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-08-10 22:43:54 +0300

    storage refactor

--- cmd/server/handlers/metrics.go
+++ cmd/server/handlers/metrics.go
@@ -1,24 +1,24 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"text/template"
 
 	"github.com/go-chi/chi/v5"
 
 	"github.com/horseinthesky/metricsagent/cmd/server/storage"
 )
 
+var stash = &storage.Memory{}
+
 func unsupportedType(mtype string) bool {
 	if mtype != "gauge" && mtype != "counter" {
-		return false
+		return true
 	}
 
-	return true
+	return false
 }
 
 func SaveMetric(w http.ResponseWriter, r *http.Request) {
@@ -26,36 +26,17 @@ func SaveMetric(w http.ResponseWriter, r *http.Request) {
 	metricName := chi.URLParam(r, "metricName")
 	valueString := chi.URLParam(r, "value")
 
-	if ok := unsupportedType(metricType); !ok {
+	if unsupportedType(metricType) {
 		w.WriteHeader(http.StatusNotImplemented)
 		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
 		return
 	}
 
-	if metricType == "gauge" {
-		value, err := strconv.ParseFloat(valueString, 64)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(http.StatusText(http.StatusBadRequest)))
-			return
-		}
-		storage.GaugeStorage[metricName] = value
-	}
-
-	if metricType == "counter" {
-		value, err := strconv.ParseInt(valueString, 10, 64)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(http.StatusText(http.StatusBadRequest)))
-			return
-		}
-
-		if oldValue, ok := storage.CounterStorage[metricName]; ok {
-			newValue := oldValue + value
-			storage.CounterStorage[metricName] = newValue
-		} else {
-			storage.CounterStorage[metricName] = value
-		}
+	err := stash.Set(metricName, valueString)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(http.StatusText(http.StatusBadRequest)))
+		return
 	}
 
 	w.Write([]byte("Received a POST request\n"))
@@ -65,28 +46,20 @@ func LoadMetric(w http.ResponseWriter, r *http.Request) {
 	metricType := chi.URLParam(r, "metricType")
 	metricName := chi.URLParam(r, "metricName")
 
-	if ok := unsupportedType(metricType); !ok {
+	if unsupportedType(metricType) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(http.StatusText(http.StatusNotFound)))
 		return
 	}
 
-	if metricType == "gauge" {
-		if value, ok := storage.GaugeStorage[metricName]; ok {
-			w.Write([]byte(fmt.Sprint(value)))
-			return
-		}
+	value, err := stash.Get(metricName)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(http.StatusText(http.StatusNotFound)))
+		return
 	}
 
-	if metricType == "counter" {
-		if value, ok := storage.CounterStorage[metricName]; ok {
-			w.Write([]byte(fmt.Sprint(value)))
-			return
-		}
-	}
-
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte(http.StatusText(http.StatusNotFound)))
+	w.Write([]byte(value))
 }
 
 func Null(w http.ResponseWriter, r *http.Request) {
@@ -95,20 +68,16 @@ func Null(w http.ResponseWriter, r *http.Request) {
 }
 
 func AllMetricHandler(w http.ResponseWriter, r *http.Request) {
-	allMetrics := map[string]float64{}
-	for k, v := range storage.GaugeStorage {
-		allMetrics[k] = v
-	}
-	for k, v := range storage.CounterStorage {
-		allMetrics[k] = float64(v)
-	}
+	allMetrics := stash.GetAll()
 
 	htmlPage, err := os.ReadFile("cmd/server/templates/dashboard.html")
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
+
 	w.Header().Set("Content-Type", "text/html")
+
 	tmpl := template.Must(template.New("").Parse(string(htmlPage)))
 	tmpl.Execute(w, allMetrics)
 }
--- cmd/server/storage/metrics.go
+++ cmd/server/storage/metrics.go
@@ -1,4 +1,77 @@
 package storage
 
-var GaugeStorage = map[string]float64{}
-var CounterStorage = map[string]int64{}
+import (
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+// var GaugeStorage = map[string]float64{}
+// var CounterStorage = map[string]int64{}
+
+type Memory struct {
+	sync.Map
+}
+
+func (m *Memory) Set(name, value string) error {
+	counter, err := strconv.ParseInt(value, 10, 64)
+	if err == nil {
+		oldValue, loaded := m.Load(name)
+		if loaded {
+			oldCounter, _ := oldValue.(int64)
+			m.Store(name, counter + oldCounter)
+			return nil
+
+		}
+
+		m.Store(name, counter)
+		return nil
+	}
+
+	gauge, err := strconv.ParseFloat(value, 64)
+	if err == nil {
+		m.Store(name, gauge)
+		return nil
+	}
+
+	return fmt.Errorf("failed to store")
+}
+
+func (m *Memory) Get(name string) (string, error) {
+	value, loaded := m.Load(name)
+	if !loaded {
+		return "", fmt.Errorf("no value found")
+	}
+
+	counter, ok := value.(int64)
+	if ok {
+		return fmt.Sprint(counter), nil
+	}
+
+	gauge, ok := value.(float64)
+	if ok {
+		return fmt.Sprint(gauge), nil
+	}
+
+	return "", fmt.Errorf("unknown type")
+}
+
+func (m *Memory) GetAll() map[string]float64 {
+	res := map[string]float64{}
+	m.Range(func(key, value interface{}) bool {
+		gauge, ok := value.(float64)
+		if ok {
+			res[key.(string)] = gauge
+			return true
+		}
+
+		counter, ok := value.(int64)
+		if ok {
+			res[key.(string)] = float64(counter)
+		}
+
+		return true
+	})
+
+	return res
+}
