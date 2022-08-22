cc68ac661b67c184a7c4cf0e8da7211ad3e84159
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-08-22 22:52:45 +0300

    env priority agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"runtime"
 	"time"
 
@@ -11,26 +13,42 @@ import (
 	"github.com/horseinthesky/metricsagent/internal/agent"
 )
 
-type Config struct {
-	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
-}
-
 var (
-	cfg  Config
-	data = &runtime.MemStats{}
+	address        *string
+	reportInterval *time.Duration
+	pollInterval   *time.Duration
+	cfg            = &agent.Config{}
+	data           = &runtime.MemStats{}
 )
 
-func init() {
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatal(fmt.Errorf("failed to parse env vars: %w", err))
+func overrideConfig(cfg *agent.Config) {
+	if _, present := os.LookupEnv("ADDRESS"); !present {
+		cfg.Address = *address
+	}
+	if _, present := os.LookupEnv("REPORT_INTERVAL"); !present {
+		cfg.ReportInterval = *reportInterval
+	}
+	if _, present := os.LookupEnv("POLL_INTERVAL"); !present {
+		cfg.PollInterval = *pollInterval
 	}
 }
 
+func init() {
+	err := env.Parse(cfg)
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to parse env vars: %w", err))
+	}
+
+	address = flag.String("a", "localhost:8080", "Address for sending data to")
+	reportInterval = flag.Duration("r", time.Duration(10*time.Second), "Metric report to server interval")
+	pollInterval = flag.Duration("p", time.Duration(2*time.Second), "Metric poll interval")
+	flag.Parse()
+
+	overrideConfig(cfg)
+}
+
 func main() {
-	agent := agent.New(cfg.PollInterval, cfg.ReportInterval, cfg.Address)
+	agent := agent.New(cfg)
 
 	for {
 		select {
--- internal/agent/main.go
+++ internal/agent/main.go
@@ -15,6 +15,12 @@ import (
 
 type gauge float64
 
+type Config struct {
+	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
+}
+
 type Agent struct {
 	PollTicker   *time.Ticker
 	ReportTicker *time.Ticker
@@ -31,18 +37,16 @@ type Metric struct {
 	Value *gauge `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
 
-func New(poll, report time.Duration, url string) *Agent {
-	agent := &Agent{
-		PollTicker:   time.NewTicker(poll),
-		ReportTicker: time.NewTicker(report),
+func New(cfg *Config) *Agent {
+	return &Agent{
+		PollTicker:   time.NewTicker(cfg.PollInterval),
+		ReportTicker: time.NewTicker(cfg.ReportInterval),
 		metrics:      &sync.Map{},
-		upstream:     fmt.Sprintf("http://%s", url),
+		upstream:     fmt.Sprintf("http://%s", cfg.Address),
 		client: &http.Client{
 			Timeout: 1 * time.Second,
 		},
 	}
-
-	return agent
 }
 
 func (a *Agent) UpdateMetrics(data *runtime.MemStats) {
