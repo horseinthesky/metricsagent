23c132ebab2ac043c07a0a50dc2c50f77c9a0dc1
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-08-22 19:52:43 +0300

    server store interval sec

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,10 +16,10 @@ import (
 )
 
 type Config struct {
-	Address       string `env:"ADDRESS" envDefault:"localhost:8080"`
-	StoreInterval int    `env:"STORE_INTERVAL" envDefault:"300"`
-	StoreFile     string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
-	Restore       bool   `env:"RESTORE" envDefault:"true"`
+	Address       string        `env:"ADDRESS" envDefault:"localhost:8080"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
+	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+	Restore       bool          `env:"RESTORE" envDefault:"true"`
 }
 
 type Server struct {
