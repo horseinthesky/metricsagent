3e4db3dc537813bec64eabdd88e9b15dcc61eb30
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-09-03 12:05:06 +0300

    inc10 ctx

---
+++ internal/server/db.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func (s *Server) runDB(ctx context.Context) {
+	db, err := sql.Open("pgx", s.config.DatabaseDSN)
+	if err != nil {
+		log.Printf("failed to open DB connection: %s", err)
+		return
+	}
+
+	s.db = db
+
+	<-ctx.Done()
+
+	s.db.Close()
+	log.Println("DB connection closed")
+	return
+}
