aa8430fa78e98af053e45ce0b01ee54b25af1dfa
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-11-19 18:19:18 +0300

    server example

---
+++ internal/server/server_example_test.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func Example() {
+	// Start server
+	cfg, err := ParseConfig()
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to parse server config: %w", err))
+	}
+
+	server := NewServer(cfg)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go server.Run(ctx)
+
+	// Handle graceful shutdown
+	term := make(chan os.Signal, 1)
+	signal.Notify(term, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	sig := <-term
+	log.Printf("signal received: %v; terminating...\n", sig)
+
+	cancel()
+	server.Stop()
+}
